43bf99516e5acde11fc6fb2c4063410678d2752c
devchoplife <dritedurojaiye@gmail.com>
2022-01-03 10:04:06 +0100

    Add linked list notes

---
+++ linked-lists/notes.go
@@ -0,0 +1,20 @@
+package linkedlists
+
+/*
+linked list is a linear collection of data elements whose order is not given by their
+physical placement in memory. Instead, each element points to the next. It is a data
+structure consisting of a collection of nodes which together represent a sequence
+
+Linked lists are not indexed unlike arrays
+
+If we need to change an element in a linked list, we need to go to the head first and then
+follow along the linked list which will be much slower (Slower O(n))
+
+Adding and deleting elements at the begining of the list will be pretty fast - faster O(n)
+This is the edge a linked list has over an array. An array has to shift.
+
+Singly linked lists contain the value and the pointer to the next value
+
+Doubly linked list contain the value, the pointer to the next value and the pointer to the
+previous value
+*/
