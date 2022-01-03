51db28bc6e9f4f33f60328632a7951d2c412a13b
devchoplife <dritedurojaiye@gmail.com>
2022-01-03 11:34:48 +0100

    Update linked lists

--- linked-lists/sll.go
+++ linked-lists/sll.go
@@ -22,6 +22,49 @@ func (l *linkedList) prepend(nodeToPrepend *node) {
 	l.length++
 }
 
+// print all the data in the list
+func (l linkedList) printListData() {
+	toPrint := l.head
+	for l.length != 0 {
+		fmt.Printf("%d ", toPrint.data)
+		toPrint = toPrint.next
+		l.length--
+	}
+	fmt.Printf("\n")
+}
+
+func (l *linkedList) deleteWithValue(value int) {
+	// handling special cases so we do not get segmentation violation error/runtime error
+
+	// empty list
+	if l.length == 0 {
+		return
+	}
+
+	// value to delete is the head
+	if value == l.head.data {
+		l.head = l.head.next
+		l.length--
+		return
+	}
+
+	previousToDelete := l.head
+
+	// if value does not exist
+	for previousToDelete.next.data != value {
+		if previousToDelete.next.next == nil {
+			return
+		}
+	}
+
+	for previousToDelete.next.data != value {
+		previousToDelete = previousToDelete.next
+	}
+
+	previousToDelete.next = previousToDelete.next.next
+	l.length--
+}
+
 func main() {
 	myList := linkedList{}
 
@@ -30,12 +73,21 @@ func main() {
 	node2 := &node{data: 24}
 	node3 := &node{data: 25}
 	node4 := &node{data: 26}
+	node5 := &node{data: 27}
+	node6 := &node{data: 28}
 
 	// prepend nodes
 	myList.prepend(node1)
 	myList.prepend(node2)
 	myList.prepend(node3)
 	myList.prepend(node4)
+	myList.prepend(node5)
+	myList.prepend(node6)
 
-	fmt.Println(myList)
+	// before deleting
+	myList.printListData()
+
+	myList.deleteWithValue(27)
+	// After deleting
+	myList.printListData()
 }
