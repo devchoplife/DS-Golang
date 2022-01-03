72a62794c3ab8eecec6a27515c40dbc52e13bed9
devchoplife <dritedurojaiye@gmail.com>
2022-01-03 15:38:34 +0100

    Add Binary tree notes

---
+++ binary-search-trees/notes.go
@@ -0,0 +1,13 @@
+package binarysearchtrees
+
+/*
+Binary tree is a tree that has nodes with not more than two children
+
+When the smaller child is placed on the left and the larger child is placed on the right,
+it is called a binary search tree
+
+Nodes are always nserted as leaves
+
+Advantage of binary tree is speed
+Really useful for a large amount of data
+*/
