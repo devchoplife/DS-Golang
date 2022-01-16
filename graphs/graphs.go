dc8a6790fbe95c4eaa6585200e6759721e7ba382
devchoplife <dritedurojaiye@gmail.com>
2022-01-16 23:24:03 +0100

    Add code for graphs

--- graphs/graphs.go
+++ graphs/graphs.go
@@ -1,5 +1,89 @@
 package main
 
-func main() {
+import "fmt"
 
+// Graph represent an adjacency list graph
+type Graph struct {
+	vertices []*Vertex
+}
+
+// Vertex represents a graph vertex
+type Vertex struct {
+	key      int
+	adjacent []*Vertex
+}
+
+// Print will print the adjacent list for each vertex of the graph
+func (g *Graph) Print() {
+	for _, v := range g.vertices {
+		fmt.Printf("\nVertex %v : ", v.key)
+		for _, v := range v.adjacent {
+			fmt.Printf(" %v ", v.key)
+		}
+	}
+	fmt.Println()
+}
+
+// AddVertex adds a vertex to the graph
+func (g *Graph) AddVertex(k int) {
+	if contains(g.vertices, k) {
+		err := fmt.Errorf("Vertex %v not added because it is an existing key", k)
+		fmt.Println(err.Error())
+	} else {
+		g.vertices = append(g.vertices, &Vertex{key: k})
+	}
+}
+
+// AddEdge adds an edge to the graph
+func (g *Graph) AddEdge(from, to int) {
+	// get vertex
+	fromVertex := g.getVertex(from)
+	toVertex := g.getVertex(to)
+
+	// check errors
+	if fromVertex == nil || toVertex == nil {
+		err := fmt.Errorf("invalid edge (%v-->%v)", from, to)
+		fmt.Println(err.Error())
+	} else if contains(fromVertex.adjacent, to) {
+		err := fmt.Errorf("existing edge (%v-->%v)", from, to)
+		fmt.Println(err.Error())
+	} else {
+		// add edge
+		fromVertex.adjacent = append(fromVertex.adjacent, toVertex)
+	}
+}
+
+//GetVertex
+func (g *Graph) getVertex(k int) *Vertex {
+	for i, v := range g.vertices {
+		if v.key == k {
+			return g.vertices[i]
+		}
+	}
+	return nil
+}
+
+// Contains
+func contains(s []*Vertex, k int) bool {
+	for _, v := range s {
+		if k == v.key {
+			return true
+		}
+	}
+	return false
+}
+
+func main() {
+	test := &Graph{}
+
+	for i := 0; i < 5; i++ {
+		test.AddVertex(i)
+	}
+
+	test.AddEdge(1, 2)
+	test.AddEdge(1, 2)
+	test.AddEdge(6, 2)
+	test.AddEdge(3, 2)
+	test.AddEdge(3, 2)
+	test.Print()
 }
